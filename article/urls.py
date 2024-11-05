9da1e666418cd8f547f012e9872f44bfb922dd6e
joelselemuka <joelselemuka100@gmail.com>
2024-11-05 10:44:55 +0200

    finish category app

--- article/urls.py
+++ article/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-
 from .views import *
+from django.urls import path
 app_name = 'article'
-urlpatterns = [ 
-    path("list/", product_list, name="product_list"),
-    path("add/", add_product, name="add_product"),
+urlpatterns = [
+    path('',product_view,name="product_list_view"),
+    path('edit/<int:pk>/',Product_update_view,name="product_update_view"),
+
+    path('delete/<int:pk>/',Product_delete_view,name="product_delete_view"),
+    path('add/',product_add_view,name="product_add_view"),
+
+    
 ]
--- article/views.py
+++ article/views.py
@@ -1,29 +1,75 @@
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
+from stock.models import Stock
+from .form import ProductForm
 from .models import Product
-
-# Create your views here.
-
-# BLOCK UNITY
-
-from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
 
 
-# END BLOCK UNITY
-
-
-
-
-def product_list(request):
+def product_view(request):
+    products=Product.objects.all().order_by('libelle')
+    articles = Stock.objects.all().values('qty','product__pk','product__libelle','product__unity','product__qteEnVente','product__productType','product__stockAlert','product__categorie','product__status')
+    form = ProductForm()
     context={
-        'page':"product",
-        'products': Product.objects.all().order_by("Name")
+        'Products':articles,
+        'page':'product',
+        'form':form,
     }
     return render(request,'article/product_list.html',context)
 
 
-def add_product(request):
-    context={
-        'page':"add_product",
+def product_add_view(request):
+    products = Product.objects.all().order_by('libelle')
+    form = ProductForm()
+    context = {
+        'form': form,
+        'products': products,
+        'page': 'add_product'
     }
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, " Article Ajouté avec succès")
+            return redirect('article:article_list_view')
+        messages.error(request, form.errors)
+        return redirect('article:article_list_view')
+    return render(request, 'article/article_list.html', context)
+
+
+def Product_update_view(request, pk):
+    cat=Product.objects.get(pk=pk)
+    Products=Product.objects.all().order_by('libelle')
+    form = ProductForm(request.POST or None, instance=cat)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(request,"modification éffectuée avec succès")
+            return redirect('Product:Product_list_view')
+        messages.error(request, "Modification échouée verifier les données entrées")
+        return redirect('Product:Product_list_view')
+    context = {
+        'form': form,
+    }
+
     return render(request,'article/add_product.html',context)
+
+
+
+def Product_delete_view(request,pk):
+    cat=Product.objects.get(pk=pk).delete()
+   
+    Products=Product.objects.all().order_by('libelle')
+    context={
+        'products':Products,
+        'page':'Product'
+    }
+    
+    return HttpResponseRedirect(reverse('Product:Product_list_view'))  
+   
+    
 
 
